6efcd67ec9fd5d80ccdf8a59d3419d4397459033
Deepak Kumar <deepak.kumar07@nagarro.com>
2022-05-30 18:36:01 +0530

    first commit

--- myjs.js
+++ myjs.js
@@ -1,1 +1,45 @@
 console.log('js ruuning in external js file.');
+
+// DOM - Document Object Model
+
+// variables
+
+// number
+// Boolean
+// String
+
+
+// Array
+// Object
+
+
+// functions
+
+
+
+// window -browser - BOM
+// document - webpage - DOM
+
+// window.document
+
+let heading = document.getElementById('heading').innerText;
+console.log(heading);
+
+document.getElementById('heading').innerText = 'New Header';
+
+
+
+
+function clickfn() {
+    console.log('button on main content got clicked....');
+}
+
+
+// Assignemnt :-
+// create a form - how to prevent default form submission
+// pick all the alues from form on button click - RadioNodeList, checkbox
+// create element
+// fill values in element
+// handling attributes in js
+// appending in js
+// attach on actual DOM
